8bf826f60cb2058872ce9cda6a1a913287269944
maldonado <maldonado@DESKTOP-P5L3H8K>
2022-03-31 18:54:52 -0600

    Add project files.

---
+++ MorsodItalia.BL/Categoria.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MorsodItalia.BL
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Ingrese la Categoría")]
+        public string Descripcion { get; set; }
+    }
+}
